8df50c5eb770e721fc73eaf623804d9a07197204
Henus321 <tyrant321@yandex.ru>
2022-12-08 16:21:32 +0300

    backend: Order default model/controller/routes added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,5 @@
 const Order = require("../models/orderModel");
 const asyncHandler = require("express-async-handler");
-const AppError = require("../utils/appError");
 
 exports.getAllOrders = asyncHandler(async (req, res, next) => {
   const orders = await Order.find();
@@ -10,26 +9,6 @@ exports.getAllOrders = asyncHandler(async (req, res, next) => {
     results: orders.length,
     data: {
       data: orders,
-    },
-  });
-});
-
-exports.createOrder = asyncHandler(async (req, res, next) => {
-  // add fields later
-  const { name } = req.body;
-
-  if (!name) {
-    return next(new AppError("Please add a name", 400));
-  }
-
-  const order = await Order.create({
-    name,
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      data: order,
     },
   });
 });
--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,31 +3,68 @@ const slugify = require("slugify");
 
 const orderSchema = new mongoose.Schema(
   {
-    name: {
+    customer: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Customer",
+      required: [true, "An order must contain a customer"],
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: [true, "An order must contain a product"],
+    },
+    assembly: {
       type: String,
-      required: [true, "An order must have a name"],
-      unique: true,
-      trim: true,
-      maxlength: [
-        40,
-        "An order name must have less or equal then 40 characters!",
-      ],
-      minlength: [
-        6,
-        "An order name must have less or equal then 40 characters!",
-      ],
+      required: [true, "An order must have an assembly"],
+    },
+    payment: {
+      type: [String],
+      required: [true, "An order must have a payment type"],
+    },
+    registration: {
+      type: Date,
+      required: [true, "An order must have a registration date"],
+    },
+    delivery: {
+      type: Date,
+      required: [true, "An order must have a delivery date"],
+    },
+    manager: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "An order must contain a manager"],
     },
     slug: String,
+    number: Number,
   },
   {
     timestamps: true,
   }
 );
 
-// Virtual populate for customers late!!!11
-
 orderSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+orderSchema.pre("save", async function (next) {
+  this.number = (await this.find().length) + 1;
+  next();
+});
+
+orderSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "customer",
+    select: "name",
+  })
+    .populate({
+      path: "product",
+      select: "brand model name photoUrl cost description",
+    })
+    .populate({
+      path: "manager",
+      select: "name",
+    });
   next();
 });
 
