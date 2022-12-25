e6a4e22c02d274d1704d21916e7467a47ffd0a09
Henus321 <tyrant321@yandex.ru>
2022-12-25 15:23:30 +0300

    frontend: cookie reminder moved to footer, styles fixed

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -1,9 +1,39 @@
-import { Layout } from "antd";
+import { Grid, Layout } from "antd";
+import CookieConsent from "react-cookie-consent";
+
 const { Footer } = Layout;
+const { useBreakpoint } = Grid;
 
 const AppFooter = () => {
+  const {} = useBreakpoint();
+
   return (
-    <Footer className="text-center">Ant Design ©2022 Created by Ant UED</Footer>
+    <>
+      <CookieConsent
+        location="bottom"
+        expires={90}
+        style={{
+          backgroundColor: "rgba(0,0,0,0.75",
+          padding: "0 50px",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "justify",
+        }}
+        buttonStyle={{
+          backgroundColor: "#1DA57A",
+          color: "#fff",
+          borderRadius: "5px",
+          padding: "8px 16px",
+        }}
+        buttonText="Accept and close"
+      >
+        We use cookies to make your experience of our websites better. By using
+        and further navigating this website you accept this.
+      </CookieConsent>
+      <Footer className="text-center">
+        Ant Design ©2022 Created by Ant UED
+      </Footer>
+    </>
   );
 };
 
