43eb574ee641065d70b66a789a58a8c9aa0dda00
Henus321 <tyrant321@yandex.ru>
2022-12-31 01:02:17 +0300

    frontend: API url added for production

--- frontend/src/constants/Routes.ts
+++ frontend/src/constants/Routes.ts
@@ -7,9 +7,13 @@ export const ORDERS_ROUTE = `${PREFIX}/orders`;
 export const CREATE_ORDER_ROUTE = `${PREFIX}/orders/create-order`;
 export const EDIT_ORDER_ROUTE = `${PREFIX}/orders/edit-order`;
 
-export const PHOTO_URL = "/api/v1/uploads/";
-export const USERS_URL = "/api/v1/users";
-export const PORTFOLIO_URL = "/api/v1/portfolio";
-export const CUSTOMERS_URL = "/api/v1/customers";
-export const PRODUCTS_URL = "/api/v1/products";
-export const ORDERS_URL = "/api/v1/orders";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://mern-dashboard-api.onrender.com"
+    : "";
+export const PHOTO_URL = `${API_URL}/api/v1/uploads/`;
+export const USERS_URL = `${API_URL}/api/v1/users`;
+export const PORTFOLIO_URL = `${API_URL}/api/v1/portfolio`;
+export const CUSTOMERS_URL = `${API_URL}/api/v1/customers`;
+export const PRODUCTS_URL = `${API_URL}/api/v1/products`;
+export const ORDERS_URL = `${API_URL}/api/v1/orders`;
