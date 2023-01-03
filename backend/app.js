32c10a1ac5166f5c605be1d5825b8046d3479314
Henus321 <tyrant321@yandex.ru>
2023-01-03 20:27:51 +0300

    frontend/backend: uploads url changed to avoid triggering logger, clean up

--- backend/app.js
+++ backend/app.js
@@ -60,11 +60,13 @@ app.use(hpp());
 app.use(compression());
 
 // Routes
-app.use("/api/v1/uploads", express.static("../frontend/public/uploads"));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/orders", orderRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/customers", customerRouter);
+
+// Static files
+app.use("/uploads", express.static("../frontend/public/uploads"));
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder up in production
@@ -74,6 +76,7 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
+// Errors
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
