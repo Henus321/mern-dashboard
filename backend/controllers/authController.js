18999e3ce9fcd47accd6907e43211da45f9785e1
Henus321 <tyrant321@yandex.ru>
2023-01-03 20:29:06 +0300

    backend: cookie options for CORS removed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,8 +16,6 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "None",
-    secure: true,
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
