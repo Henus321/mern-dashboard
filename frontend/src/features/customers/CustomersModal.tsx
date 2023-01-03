f345b1c8b2c8d30cf67fcc555c3a4f97853cf022
Henus321 <tyrant321@yandex.ru>
2023-01-03 17:34:39 +0300

    frontend: create new Customer implemented

--- frontend/src/features/customers/CustomersModal.tsx
+++ frontend/src/features/customers/CustomersModal.tsx
@@ -1,70 +1,100 @@
 import React, { useState } from "react";
 import { Button, Form, Input, Modal } from "antd";
-import { MailOutlined, LockOutlined, UserAddOutlined } from "@ant-design/icons";
-import { MAX_40 } from "../../constants";
+import { UserAddOutlined } from "@ant-design/icons";
+import { LEN_11, MAX_100, MAX_20, ONLY_NUMBERS } from "../../constants";
+import { useAppDispatch } from "../../hooks";
+import { createCustomer } from "./customersSlice";
 
 const CustomersModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const dispatch = useAppDispatch();
 
   const showModal = () => {
     setIsModalOpen(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
   const handleCancel = () => {
     setIsModalOpen(false);
+    form.resetFields();
+  };
+
+  const onFinish = () => {
+    const newCustomer = form.getFieldsValue();
+    dispatch(createCustomer(newCustomer));
   };
 
   return (
     <>
       <Modal
-        title="Basic Modal"
+        title="Create New Customer"
         open={isModalOpen}
-        onOk={handleOk}
+        onOk={form.submit}
         onCancel={handleCancel}
       >
         <Form
-          // initialValues={initialValues}
+          form={form}
           layout="vertical"
-          name="login-form"
+          name="customers-form"
+          onFinish={onFinish}
         >
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: "Please input name" }, MAX_20]}
+          >
+            <Input className="rounded" size="large" placeholder="Enter name" />
+          </Form.Item>
+          <Form.Item
+            rules={[
+              { required: true, message: "Please input phone" },
+              LEN_11,
+              ONLY_NUMBERS,
+            ]}
+            name="phone"
+            label="Phone Number"
+          >
+            <Input placeholder="89031234567" className="rounded" />
+          </Form.Item>
           <Form.Item
             name="email"
             label="Email"
             rules={[
-              { required: true, message: "Please input your email" },
+              { required: true, message: "Please input email" },
               {
                 type: "email",
                 message: "Please enter a validate email!",
               },
-              MAX_40,
+              MAX_20,
+            ]}
+          >
+            <Input className="rounded" size="large" placeholder="Enter email" />
+          </Form.Item>
+          <Form.Item
+            name="social"
+            label="Social Network"
+            rules={[
+              { required: true, message: "Please input social network" },
+              { type: "url" },
+              MAX_100,
             ]}
           >
             <Input
               className="rounded"
-              name="email"
               size="large"
-              placeholder="Your email"
-              prefix={<MailOutlined />}
+              placeholder="www.website.com"
             />
           </Form.Item>
           <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              { required: true, message: "Please input your password" },
-              MAX_40,
-            ]}
+            name="city"
+            label="City"
+            rules={[{ required: true, message: "Please input name" }, MAX_20]}
           >
-            <Input.Password
+            <Input
               className="rounded"
-              name="password"
               size="large"
-              placeholder="Password"
-              prefix={<LockOutlined />}
+              placeholder="Enter city name"
             />
           </Form.Item>
         </Form>
